a060c348751e7acf0798a86c52f617957b3c7db9
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-12-19 18:57:22 +0000

    update submit_crush path

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("verify/<uuid:code>/", views.EmailVerificationView.as_view(), name="verify"),
     path("login/", views.LoginView.as_view(), name="login"),
+    path("crush/submit", views.SubmitCrushView.as_view(), name="submit_crush"),
 ]
