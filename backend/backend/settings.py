470fab5e63573ab634b41b999a1e1d1f3d7da187
TomLam <136619817+T0mLam@users.noreply.github.com>
2025-01-22 00:05:21 +0000

    update requrirements.txt and database settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from dotenv import load_dotenv
 import django_heroku
+import dj_database_url
 from pathlib import Path
 
 
@@ -94,14 +95,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"), 
-        "PORT": os.getenv("DB_PORT"),      
-    }
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
