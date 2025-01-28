f6fc5f00266dca0e82d79b1505f033feeb89bd50
rsh <emkayhrushikesh@gmail.com>
2025-01-28 20:40:51 +0000

    Changed the base url in api.js. Next commit will tell you whether everything worked

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,10 +3,10 @@ import axios from "axios";
 // Create an axios instance with a default config for api requests
 const api = axios.create({
   //baseURL: import.meta.env.VITE_REACT_APP_API_URL,
-  baseURL: "https://bristollink-e1a5719dffba.herokuapp.com/api/",
+  baseURL: "https://bristol-link-e9adb36fa085.herokuapp.com/api/",
   headers: {
     "Content-Type": "application/json",
-  },  
+  },
 });
 
 // Add a request interceptor to add the token to the headers before making the request
