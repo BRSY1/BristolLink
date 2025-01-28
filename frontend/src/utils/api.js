8a6b96e43b85ce55371b606143d2fdcb6720ff77
rsh <emkayhrushikesh@gmail.com>
2025-01-28 20:56:15 +0000

    Tried to shift things to Europe with the env variables, got an xhr error. everything is still in the US

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create an axios instance with a default config for api requests
 const api = axios.create({
   //baseURL: import.meta.env.VITE_REACT_APP_API_URL,
-  baseURL: "https://bristol-link-e9adb36fa085.herokuapp.com/api/",
+  baseURL: "https://bristollink-e1a5719dffba.herokuapp.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
