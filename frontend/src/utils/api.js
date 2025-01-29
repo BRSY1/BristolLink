6cc293633c02f4696e21ba0d9ed195c57a728f71
rsh <emkayhrushikesh@gmail.com>
2025-01-29 12:59:43 +0000

    Made sure baseURL is not exposed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create an axios instance with a default config for api requests
 const api = axios.create({
-  //baseURL: import.meta.env.VITE_REACT_APP_API_URL,
-  baseURL: "https://bristollink-e1a5719dffba.herokuapp.com/api/",
+  baseURL: import.meta.env.VITE_REACT_APP_API_URL,
+  // baseURL: "https://bristollink-e1a5719dffba.herokuapp.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
